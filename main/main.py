452281171ed613cedcfb12fe9250381091ea0b1a
forest-book <124020491+forest-book@users.noreply.github.com>
2025-12-15 20:40:29 +0900

    定常状態の統計量を出すための設定

--- main/main.py
+++ main/main.py
@@ -269,9 +269,9 @@ class MainController:
         Plotter.plot_fused_RL_errors_from_csv(error_filename)
         
         # 統計情報の表示と保存
-        self.data_logger.print_fused_RL_error_statistics(transient_time=10.0)
-        self.data_logger.save_fused_RL_error_statistics(transient_time=10.0)
-        self.data_logger.save_fused_RL_error_statistics(transient_time=10.0, format='txt')
+        self.data_logger.print_fused_RL_error_statistics(transient_time=120.0)
+        self.data_logger.save_fused_RL_error_statistics(transient_time=120.0)
+        self.data_logger.save_fused_RL_error_statistics(transient_time=120.0, format='txt')
 
 if __name__ == '__main__':
     # 設定ファイルから読み込む
