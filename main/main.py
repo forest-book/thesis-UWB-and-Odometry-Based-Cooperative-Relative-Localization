4daf45476bfca3bf77b209f416364d27c42f9b99
forest-book <124020491+forest-book@users.noreply.github.com>
2026-01-13 18:18:27 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -288,10 +288,6 @@ class MainController:
 
 if __name__ == '__main__':
     # 設定ファイルから読み込む
-    # JSON形式
-    #simulation_params = ConfigLoader.load('../config/simulation_config.json')
-
-    # YAML形式
     simulation_params = ConfigLoader.load('../config/simulation_config.yaml')
 
     controller = MainController(simulation_params)
